ae2945a785c8b49cfd931d23f4cf120edc3af8e2
ali <straoid@gmail.com>
2024-05-14 10:12:40 +0300

    udpate doctypes

---
+++ taxi_madina_app/taxi_madina_app/doctype/madina_member/madina_member.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, CypherStream and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Madina Member", {
+// 	refresh(frm) {
+
+// 	},
+// });
