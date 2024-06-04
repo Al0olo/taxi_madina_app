d03542e6495e92e860d9e53c4b6339d237396bdf
ali <straoid@gmail.com>
2024-06-04 16:53:03 +0300

    update

--- taxi_madina_app/apis/authentication.py
+++ taxi_madina_app/apis/authentication.py
@@ -4,7 +4,7 @@ import json
 @frappe.whitelist(allow_guest=True,methods=["POST"])
 def authenticate_user(data):
     # update
-    return data
+    # return data
     res = {
         "status_code": 400,
         "message":"Sucess",
