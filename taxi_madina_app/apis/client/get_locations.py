24126124adbc3282e5bb3d73af1a848ce38a950a
ali <straoid@gmail.com>
2024-07-11 17:50:15 +0300

    update get locations

--- taxi_madina_app/apis/client/get_locations.py
+++ taxi_madina_app/apis/client/get_locations.py
@@ -4,7 +4,12 @@ import frappe
 def get_locations(location_type):
     try:
         # client = frappe.session.user
-        locations = frappe.get_all("Locations",{"location_type":location_type},{"*"})
+        if location_type == 'Inside_City':
+            locations = frappe.get_all("Locations",{"location_type":'Inside City'},{"*"})
+        elif location_type == 'outside_City':
+            locations = frappe.get_all("Locations",{"location_type":'outside City'},{"*"})
+        elif location_type == 'AirPorts':
+            locations = frappe.get_all("Locations",{"location_type":'AirPorts'},{"*"})
         return {
             "status_code": 200,
             "message": "Success",
